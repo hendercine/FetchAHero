1fa3051d59d3c9429bc8baf10448ad5a5a60f4a2
hendercine <james@slimeify.co>
2024-09-12 16:15:52 -0700

    [FAH-6]feat: add the detail screen composable

---
+++ app/src/main/java/dev/jameshenderson/fetchahero/ui/heroDetails/HeroDetailScreen.kt
@@ -0,0 +1,104 @@
+package dev.jameshenderson.fetchahero.ui.heroDetails
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HeroDetailScreen(
+    viewModel: HeroDetailViewModel,
+    navigateBack: () -> Unit = {}
+) {
+    val hero = viewModel.heroStateFlow.collectAsState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = hero.value?.title ?: "Hero Details",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navigateBack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                if (hero.value != null) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp)
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        val imageUrl = "${hero.value!!.thumbnail?.path}.${hero.value!!
+                            .thumbnail?.extension}".replace("http", "https")
+                        AsyncImage(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data(imageUrl)
+                                .crossfade(true)
+                                .build(),
+                            contentDescription = "Hero Cover Thumbnail",
+                            modifier = Modifier
+                                .padding(end = 8.dp)
+                        )
+                    }
+                    Row {
+                        Text(
+                            text = hero.value!!.description ?: "Description not found",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+
+                    hero.value!!.creators?.items?.forEach {
+                        Row {
+                            Text(
+                                text = "${it.role}: ",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                            Text(
+                                text = "${it.name}",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        }
+                    }
+                } else {
+                    Text(text = "Hero not found")
+                }
+            }
+        }
+    }
+}
