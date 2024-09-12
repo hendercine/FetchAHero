21c78b6214b8f415880e11f2c183dfeb0ab8bad0
hendercine <james@slimeify.co>
2024-09-12 12:44:58 -0700

    [FAH-2]feat: DateSerializer to format marvel dates

---
+++ app/src/main/java/dev/jameshenderson/fetchahero/data/serializers/DateSerializer.kt
@@ -0,0 +1,25 @@
+package dev.jameshenderson.fetchahero.data.serializers
+
+import androidx.core.net.ParseException
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object DateDeserializer : JsonDeserializer<Date?> {
+    private val df =
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", java.util.Locale.US)
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: java.lang.reflect.Type?,
+        context: JsonDeserializationContext?,
+    ): Date? {
+        return try {
+            json?.asString?.let { df.parse(it) }
+        } catch (e: ParseException) {
+            println("Error parsing date: ${e.message}")
+            Date()
+        }
+    }
+}
