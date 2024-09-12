0cf5668f1729a18ffef16e7f2b29890068d7606a
hendercine <james@slimeify.co>
2024-09-12 14:19:48 -0700

    [FAH-3]feat: add keys to buildConfig and add util

---
+++ app/src/main/java/dev/jameshenderson/fetchahero/utils/BuildConfigUtil.kt
@@ -0,0 +1,25 @@
+package dev.jameshenderson.fetchahero.utils
+
+import dev.jameshenderson.fetchahero.BuildConfig
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class BuildConfigUtil @Inject constructor() {
+    @Suppress("KotlinConstantConditions")
+    fun getPrivateApiKey(): String? {
+        return if (BuildConfig.PRIVATE_MARVEL_API_KEY != "empty_key") {
+            BuildConfig.PRIVATE_MARVEL_API_KEY
+        } else {
+            null
+        }
+    }
+    @Suppress("KotlinConstantConditions")
+    fun getPublicApiKey(): String? {
+        return if (BuildConfig.PRIVATE_MARVEL_API_KEY != "empty_key") {
+            BuildConfig.PUBLIC_MARVEL_API_KEY
+        } else {
+            null
+        }
+    }
+}
