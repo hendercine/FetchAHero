d5fb303d48deb0666768026f60b9869c339dc3e8
hendercine <james@slimeify.co>
2024-09-12 14:33:03 -0700

    [FAH-4]chore: add interface and hashing extension

---
+++ app/src/main/java/dev/jameshenderson/fetchahero/domain/repositories/HeroRepositoryInterface.kt
@@ -0,0 +1,8 @@
+package dev.jameshenderson.fetchahero.domain.repositories
+
+import dev.jameshenderson.fetchahero.data.models.Hero
+
+interface HeroRepositoryInterface {
+    suspend fun getHeroes(): List<Hero>
+    suspend fun getHeroDetails(comicId: Int): Hero
+}
