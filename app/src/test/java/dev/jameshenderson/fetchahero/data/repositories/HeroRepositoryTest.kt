d30ae5aff051c79e24672f3fd26a0ecd0d52a464
hendercine <james@slimeify.co>
2024-09-12 17:05:26 -0700

    [FAH-7]test: get hero detail test

--- app/src/test/java/dev/jameshenderson/fetchahero/data/repositories/HeroRepositoryTest.kt
+++ app/src/test/java/dev/jameshenderson/fetchahero/data/repositories/HeroRepositoryTest.kt
@@ -13,6 +13,7 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class HeroRepositoryTest {
@@ -20,10 +21,70 @@ class HeroRepositoryTest {
     private val marvelApi = mockk<MarvelApi>()
     private val buildConfigUtil = BuildConfigUtil()
     private val repository = HeroRepository(marvelApi, buildConfigUtil)
+    private val expectedListOfHeroes = listOf(
+        Hero(
+            id = 1,
+            title = "Fake Hero 1",
+            description = "Description 1",
+            creators = CreatorList(
+                items = listOf(
+                    CreatorSummary(
+                        name = "Stan Lee"
+                    ),
+                    CreatorSummary(
+                        name = "Len Wein"
+                    ),
+                    CreatorSummary(
+                        name = "Steve Ditko"
+                    )
+                )
+            )
+        ),
+        Hero(
+            id = 2,
+            title = "Fake Hero 2",
+            description = "Description 2",
+            creators = CreatorList(
+                items = listOf(
+                    CreatorSummary(
+                        name = "Jim Lee"
+                    ),
+                    CreatorSummary(
+                        name = "Jack Kirby"
+                    ),
+                    CreatorSummary(
+                        name = "Al Ewing"
+                    )
+                )
+            )
+        ),
+        Hero(
+            id = 3,
+            title = "Fake Hero 3",
+            description = "Description 3",
+            creators = CreatorList(
+                items = listOf(
+                    CreatorSummary(
+                        name = "Gerry Duggan"
+                    ),
+                    CreatorSummary(
+                        name = "Donny Cates"
+                    ),
+                    CreatorSummary(
+                        name = "Frank Miller"
+                    )
+                )
+            )
+        ),
+    )
+
+    @Before
+    fun setup() {
+        coEvery { marvelApi.marvelService }.returns(marvelService)
+    }
 
     @Test
     fun `getHeroes returns a list of Hero objects from repository`() = runTest {
-        coEvery { marvelApi.marvelService }.returns(marvelService)
         coEvery {
             marvelService.getHeroes(any(), any(), any(), any(), any())
         }.returns(
@@ -98,64 +159,44 @@ class HeroRepositoryTest {
             )
         )
 
-        val expected = listOf(
-            Hero(
-                id = 1,
-                title = "Fake Hero 1",
-                description = "Description 1",
-                creators = CreatorList(
-                    items = listOf(
-                        CreatorSummary(
-                            name = "Stan Lee"
-                        ),
-                        CreatorSummary(
-                            name = "Len Wein"
-                        ),
-                        CreatorSummary(
-                            name = "Steve Ditko"
-                        )
-                    )
-                )
-            ),
-            Hero(
-                id = 2,
-                title = "Fake Hero 2",
-                description = "Description 2",
-                creators = CreatorList(
-                    items = listOf(
-                        CreatorSummary(
-                            name = "Jim Lee"
-                        ),
-                        CreatorSummary(
-                            name = "Jack Kirby"
-                        ),
-                        CreatorSummary(
-                            name = "Al Ewing"
-                        )
-                    )
-                )
-            ),
-            Hero(
-                id = 3,
-                title = "Fake Hero 3",
-                description = "Description 3",
-                creators = CreatorList(
-                    items = listOf(
-                        CreatorSummary(
-                            name = "Gerry Duggan"
-                        ),
-                        CreatorSummary(
-                            name = "Donny Cates"
-                        ),
-                        CreatorSummary(
-                            name = "Frank Miller"
-                        )
-                    )
-                )
-            ),
-        )
+        val expected = expectedListOfHeroes
         val result = repository.getHeroes()
 
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `getHeroDetails returns a Hero object from repository`() = runTest {
+        coEvery {
+            marvelService.getHeroDetails(any(), any(), any(), any())
+        }.returns(
+            HeroesResponse(
+                code = 200,
+                status = "OK",
+                copyright = "Copyright",
+                attributionText = "Attribution",
+                attributionHTML = "Attribution HTML",
+                data = DataContainer(
+                    offset = 0,
+                    limit = 20,
+                    total = 3,
+                    count = 3,
+                    results = listOf(
+                        Hero(
+                            id = 1,
+                            title = "Fake Hero 1",
+                            description = "Description 1",
+                        )
+                    )
+                )
+            )
+        )
+        val expected = Hero(
+            id = 1,
+            title = "Fake Hero 1",
+            description = "Description 1",
+        )
+        val result = repository.getHeroDetails(1)
         Assert.assertEquals(expected, result)
     }
 }
